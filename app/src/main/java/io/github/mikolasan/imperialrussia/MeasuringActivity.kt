079a1dd104cfd6fac2c6c5c9de6e0a8c19b9f88d
Nikolay Neupokoev <neupokoev.n@gmail.com>
2019-06-19 21:48:55 -0700

    Finish initial redesign phase

--- app/src/main/java/io/github/mikolasan/imperialrussia/MeasuringActivity.kt
+++ app/src/main/java/io/github/mikolasan/imperialrussia/MeasuringActivity.kt
@@ -8,16 +8,62 @@ import android.widget.EditText
 
 class MeasuringActivity : Activity() {
 
-    fun convertToArshin(meters: Double): Double {
-        return meters / 0.7112
+    enum class Unit {
+        POINT,
+        LINE,
+        INCH,
+        TIP, // vershok
+        PALM, // piad
+        FOOT,
+        YARD, // arshin
+        FATHOM, // sazhen
+        TURN, // versta
+        MILE,
+    }
+
+    val ratio : DoubleArray = doubleArrayOf(
+            3.5714285714285714285714285714286e-4,
+            0.00357142857142857142857142857143,
+            0.03571428571428571428571428571429,
+            0.0625,
+            0.25,
+            0.42857142857142857142857142857143,
+            1.0,
+            3.0,
+            1500.0,
+            10500.0
+    )
+
+    private fun convertToYard(type: Unit, value: Double): Double {
+        return value / ratio[type.ordinal]
+    }
+
+    private fun convertYardTo(type: Unit, value: Double): Double {
+        return value * ratio[type.ordinal]
+    }
+
+    fun convertUnits(type: Unit, value: Double, resultType: Unit): Double {
+        if (type == resultType) {
+            return value;
+        } else if (Unit.YARD == type) {
+            return convertYardTo(resultType, value)
+        } else if (Unit.YARD == resultType) {
+            return convertToYard(type, value)
+        }
+        return .0;
+    }
+
+    fun convertToArshin(inches: Double): Double {
+        return convertYardTo(Unit.YARD, convertToYard(Unit.INCH, inches))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_measuring)
-        val input = findViewById<EditText>(R.id.editInput)
-        val output = findViewById<EditText>(R.id.editOutput)
-        input.addTextChangedListener(object: TextWatcher {
+
+        val convFromInput = findViewById<EditText>(R.id.conv_from_input)
+        val convToInput = findViewById<EditText>(R.id.conv_to_input)
+        convToInput.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
 
@@ -26,12 +72,11 @@ class MeasuringActivity : Activity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s?.isNotEmpty()!!) {
-                    val meters = s.toString().toDouble()
-                    val arshin = convertToArshin(meters)
-                    output.setText(arshin.toString())
+                    val inches = s.toString().toDouble()
+                    val arshin = convertToArshin(inches)
+                    convFromInput.setText(arshin.toString())
                 }
             }
         })
-        //input.setText()
     }
 }
