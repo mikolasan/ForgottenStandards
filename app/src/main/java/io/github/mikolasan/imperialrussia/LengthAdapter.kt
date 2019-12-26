c26f94cea58bafa8157520ff0365dd95010af70a
Nikolay Neupokoev <neupokoev.n@gmail.com>
2019-12-25 21:20:04 -0800

    Optimize getView reusing existing views

--- app/src/main/java/io/github/mikolasan/imperialrussia/LengthAdapter.kt
+++ app/src/main/java/io/github/mikolasan/imperialrussia/LengthAdapter.kt
@@ -1,14 +1,59 @@
 package io.github.mikolasan.imperialrussia
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.text.ParsePosition
 
 class LengthAdapter(context: Context, private val units: Array<ImperialUnit>) : BaseAdapter() {
     private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val getStringFromResourceId: (Int) -> String = { i: Int -> context.resources.getString(i) }
+    private val getDrawableFromResourceId: (Int) -> Drawable = { i: Int ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // >= (API 21) Android 5.0 Lollipop
+            context.resources.getDrawable(i, null)
+        } else {
+            @Suppress("DEPRECATION")
+            context.resources.getDrawable(i)
+        }
+    }
+    private val getColorFromRsourceId: (Int) -> Int = { i: Int ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // >= (API 23) Android 6.0 Marshmallow
+            val theme = null
+            context.resources.getColor(i, theme)
+        } else {
+            @Suppress("DEPRECATION")
+            context.resources.getColor(i)
+        }
+    }
+
+    enum class ViewState {
+        FROM,
+        TO,
+        OTHER
+    }
+
+    val backgrounds = mapOf(
+        ViewState.FROM to R.drawable.ic_side_from_back,
+        ViewState.TO to R.drawable.ic_side_to_back,
+        ViewState.OTHER to R.drawable.ic_side_panel_back
+    )
+    val nameColors = mapOf(
+        ViewState.FROM to R.color.inputNormal,
+        ViewState.TO to R.color.inputSelected,
+        ViewState.OTHER to R.color.colorPrimary
+    )
+    val valueColors = mapOf(
+        ViewState.FROM to R.color.keyboardDigit,
+        ViewState.TO to R.color.keyboardDigit,
+        ViewState.OTHER to R.color.keyboardDigit
+    )
 
     private lateinit var masterUnit: ImperialUnit
     private var masterValue: Double = 0.0
@@ -34,17 +79,47 @@ class LengthAdapter(context: Context, private val units: Array<ImperialUnit>) : 
         toUnit = unit
     }
 
-    override fun getView(position: Int, contentView: View?, parent: ViewGroup?): View {
-        val view = when (units[position]) {
-            fromUnit -> inflater.inflate(R.layout.listview_from_item, parent, false)
-            toUnit -> inflater.inflate(R.layout.listview_to_item, parent, false)
-            else -> inflater.inflate(R.layout.listview_item, parent, false)
+    private fun updateViewColors(layout: ConstraintLayout, dataPosition: Int) {
+        val data: ImperialUnit = getItem(dataPosition) as ImperialUnit
+        val nameTextView: TextView = layout.findViewById(R.id.unit_name)
+        when (data) {
+            fromUnit -> {
+                layout.background = getDrawableFromResourceId(backgrounds.getValue(ViewState.FROM))
+                nameTextView.setTextColor(getColorFromRsourceId(nameColors.getValue(ViewState.FROM)))
+            }
+            toUnit -> {
+                layout.background = getDrawableFromResourceId(backgrounds.getValue(ViewState.TO))
+                nameTextView.setTextColor(getColorFromRsourceId(nameColors.getValue(ViewState.TO)))
+            }
+            else -> {
+                layout.background = getDrawableFromResourceId(backgrounds.getValue(ViewState.OTHER))
+                nameTextView.setTextColor(getColorFromRsourceId(nameColors.getValue(ViewState.OTHER)))
+            }
         }
-        val nameTextView: TextView = view.findViewById(R.id.unit_name)
-        nameTextView.text = inflater.context.resources.getString(units[position].resourceId)
-        val valueTextView: TextView = view.findViewById(R.id.unit_value)
-        valueTextView.text = valueForDisplay(units[position].value)
-        return view
+    }
+    private fun updateViewData(layout: ConstraintLayout, dataPosition: Int) {
+        val data: ImperialUnit = getItem(dataPosition) as ImperialUnit
+        val nameTextView: TextView = layout.findViewById(R.id.unit_name)
+        nameTextView.text = getStringFromResourceId(data.resourceId)
+        val valueTextView: TextView = layout.findViewById(R.id.unit_value)
+        valueTextView.text = valueForDisplay(data.value)
+    }
+
+    override fun getView(position: Int, contentView: View?, parent: ViewGroup?): View {
+        if (contentView != null) {
+            if (contentView is ConstraintLayout) {
+                updateViewData(contentView, position)
+                updateViewColors(contentView, position)
+                return contentView
+            }
+            return contentView
+        } else {
+            val view = inflater.inflate(R.layout.listview_item, parent, false) as ConstraintLayout
+            updateViewData(view, position)
+            updateViewColors(view, position)
+            return view
+        }
+
     }
 
     override fun getItem(position: Int): Any {
